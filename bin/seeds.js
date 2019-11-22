9bc8b3d2a789b1e65b90ea380489d9e554c95466
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-11-22 09:36:35 +0100

    Friday morning

--- bin/seeds.js
+++ bin/seeds.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const giftModel = require("./../models/product");
 const gifts = require("./../database");
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
